d2f89b6f5f7c41bad0e1e481d7a8a1deddd11289
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-07-26 20:12:35 +0100

    food page commit

--- src/app/api/foods/[name]/route.ts
+++ src/app/api/foods/[name]/route.ts
@@ -1,16 +1,19 @@
 import { foods } from "@/data";
+import { use } from "react";
 
 export async function GET(
   request: Request,
-  { params }: { params: { name: string } }
+  { params }: { params: Promise<{ name: string }> }
 ) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const {name} = use(params);
   const index = foods.findIndex(
-    (food) => food.name.toLowerCase().replace(/ /g, "-") === params.name
+    (food) => food.name.toLowerCase().replace(/ /g, "-") === name
   );
   if (index != -1) {
     return new Response(JSON.stringify(foods[index]), {
       headers: {
-        "content type": "application/json",
+        "content-type": "application/json",
       },
       status: 200,
     });
@@ -19,7 +22,7 @@ export async function GET(
       new Response("Food not found"),
       {
         headers: {
-          "content type": "application/json",
+          "content-type": "application/json",
         },
         status: 404,
       }
--- src/app/food/[name]/page.tsx
+++ src/app/food/[name]/page.tsx
@@ -1,49 +1,50 @@
 "use client";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import { IFood, IMacronutrientData } from "@/types";
 import { useRouter } from "next/navigation";
 import { Undo2 } from "lucide-react";
 import { Cell, Pie, PieChart } from "recharts";
 
-const FoodPage = ({ params }: { params: { name: string } }) => {
+const FoodPage = ({ params }: { params: Promise<{ name: string }> }) => {
+  const { name } = use(params);
   const router = useRouter();
   const [food, setFood] = useState<IFood | null>(null);
-  const [isLoading , setIsLoading]= useState<boolean>(true);
-  const [macronutrinments, setMacronutrinments] = useState<IMacronutrientData[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [macronutrinments, setMacronutrinments] = useState<
+    IMacronutrientData[]
+  >([]);
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
 
-  const fetchFood = async() => {
-    try{
-        const APIQueryURL = `/api/foods/${params.name}`
-        const response = await fetch(APIQueryURL)
-        const data = await response.json();
+  const fetchFood = async () => {
+    try {
+      const APIQueryURL = `/api/foods/${name}`;
+      const response = await fetch(APIQueryURL);
+      const data = await response.json();
 
-        // Macronutriments
-        const macronutrimentsDatas: IMacronutrientData[] = [
-            {name: 'carbohydrates', value: data.carbohydrates},
-            {name: 'protein', value: data.protein},
-            {name: 'fat', value: data.fat},
-        ]
-        setMacronutrinments(macronutrimentsDatas);
+      // Macronutriments
+      const macronutrimentsDatas: IMacronutrientData[] = [
+        { name: "carbohydrates", value: data.carbohydrates },
+        { name: "protein", value: data.protein },
+        { name: "fat", value: data.fat },
+      ];
+      setMacronutrinments(macronutrimentsDatas);
 
-        //Food general
-        setFood(data);
-        
-    }catch(error){
-        console.log(error);
-    } finally{
-        setIsLoading(false);
+      //Food general
+      setFood(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-  }
-
-  useEffect(()=>{
-    const initialize = async () =>{
-        await fetchFood();
-        initialize();
-    }
-  },[])
+  useEffect(() => {
+    const initialize = async () => {
+      await fetchFood();
+    };
+    initialize();
+  }, [name]);
 
   return (
     <PieChart width={800} height={400}>
