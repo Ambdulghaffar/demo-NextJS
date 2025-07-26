ef765d2fa4cf34783941662e4713b16b20d76521
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-07-26 16:35:46 +0100

    home page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,97 @@
+"use client";
+
+import { IFoodReduced, IFood } from "@/types";
+
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+
 export default function Home() {
-  return <p>welcome</p>;
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const [foods, setFoods] = useState<IFoodReduced[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const fetchFoods = async () => {
+    try {
+      const response = await fetch("/api/foods/all");
+      const data = await response.json();
+      const foodReduced: IFoodReduced[] = data.map((food: IFood) => ({
+        value: food.name.toLowerCase().replace(/ /g, "-"),
+        label: food.name,
+      }));
+      setFoods(foodReduced);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    const initialize = async()=>{
+      await fetchFoods();
+      setIsLoading(false)
+    }
+    initialize();
+  },[])
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? foods.find((food) => food.value === value)?.label
+            : "Select food..."}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search food..." className="h-9" />
+          <CommandList>
+            <CommandEmpty>No food found.</CommandEmpty>
+            <CommandGroup>
+              {foods.map((food) => (
+                <CommandItem
+                  key={food.value}
+                  value={food.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  {food.label}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === food.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
 }
