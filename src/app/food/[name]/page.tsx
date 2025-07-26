9f38412631d5976b35e0291813a8a676507c2439
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-07-26 19:52:17 +0100

    food page commit

--- src/app/food/[name]/page.tsx
+++ src/app/food/[name]/page.tsx
@@ -2,23 +2,53 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { IFood, IMacronutrientData } from "@/types";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { Undo2 } from "lucide-react";
 import { Cell, Pie, PieChart } from "recharts";
 
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-
 const FoodPage = ({ params }: { params: { name: string } }) => {
+  const router = useRouter();
+  const [food, setFood] = useState<IFood | null>(null);
+  const [isLoading , setIsLoading]= useState<boolean>(true);
+  const [macronutrinments, setMacronutrinments] = useState<IMacronutrientData[]>([])
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
+
+  const fetchFood = async() => {
+    try{
+        const APIQueryURL = `/api/foods/${params.name}`
+        const response = await fetch(APIQueryURL)
+        const data = await response.json();
+
+        // Macronutriments
+        const macronutrimentsDatas: IMacronutrientData[] = [
+            {name: 'carbohydrates', value: data.carbohydrates},
+            {name: 'protein', value: data.protein},
+            {name: 'fat', value: data.fat},
+        ]
+        setMacronutrinments(macronutrimentsDatas);
+
+        //Food general
+        setFood(data);
+        
+    }catch(error){
+        console.log(error);
+    } finally{
+        setIsLoading(false);
+    }
+
+  }
+
+  useEffect(()=>{
+    const initialize = async () =>{
+        await fetchFood();
+        initialize();
+    }
+  },[])
+
   return (
     <PieChart width={800} height={400}>
       <Pie
-        data={data}
+        data={macronutrinments}
         cx={120}
         cy={200}
         innerRadius={60}
@@ -27,26 +57,7 @@ const FoodPage = ({ params }: { params: { name: string } }) => {
         paddingAngle={5}
         dataKey="value"
       >
-        {data.map((entry, index) => (
-          <Cell
-            key={`cell-${entry.name}`}
-            fill={COLORS[index % COLORS.length]}
-          />
-        ))}
-      </Pie>
-      <Pie
-        data={data}
-        cx={420}
-        cy={200}
-        startAngle={180}
-        endAngle={0}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={5}
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
+        {macronutrinments.map((entry, index) => (
           <Cell
             key={`cell-${entry.name}`}
             fill={COLORS[index % COLORS.length]}
