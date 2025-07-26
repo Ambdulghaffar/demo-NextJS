ae29f22dc5e9a753f4a61a8a3f417c0bc80e2c9f
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-07-26 17:47:38 +0100

    food page commit

---
+++ src/app/food/[name]/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { IFood,IMacronutrientData } from "@/types";
+import { useRouter } from "next/router";
+import { Undo2 } from "lucide-react";
+
+const FoodPage = ({params }: {params: {name: string}})=>{
+    
+}
+
+export default FoodPage;
