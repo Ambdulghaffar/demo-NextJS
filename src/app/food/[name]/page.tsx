fdd713aba4812318dcc81b3ec2045245d612ab05
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-07-26 18:30:15 +0100

    food page commit

--- src/app/food/[name]/page.tsx
+++ src/app/food/[name]/page.tsx
@@ -1,12 +1,60 @@
-'use client';
+"use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { IFood,IMacronutrientData } from "@/types";
+import { IFood, IMacronutrientData } from "@/types";
 import { useRouter } from "next/router";
 import { Undo2 } from "lucide-react";
+import { Cell, Pie, PieChart } from "recharts";
 
-const FoodPage = ({params }: {params: {name: string}})=>{
-    
-}
+const data = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const FoodPage = ({ params }: { params: { name: string } }) => {
+  return (
+    <PieChart width={800} height={400}>
+      <Pie
+        data={data}
+        cx={120}
+        cy={200}
+        innerRadius={60}
+        outerRadius={80}
+        fill="#8884d8"
+        paddingAngle={5}
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell
+            key={`cell-${entry.name}`}
+            fill={COLORS[index % COLORS.length]}
+          />
+        ))}
+      </Pie>
+      <Pie
+        data={data}
+        cx={420}
+        cy={200}
+        startAngle={180}
+        endAngle={0}
+        innerRadius={60}
+        outerRadius={80}
+        fill="#8884d8"
+        paddingAngle={5}
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell
+            key={`cell-${entry.name}`}
+            fill={COLORS[index % COLORS.length]}
+          />
+        ))}
+      </Pie>
+    </PieChart>
+  );
+};
 
 export default FoodPage;
