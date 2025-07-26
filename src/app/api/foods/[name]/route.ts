6fe3e81de88e1a1eab5804745686dfb294ea91b5
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-07-26 20:23:33 +0100

    food page commit

--- src/app/api/foods/[name]/route.ts
+++ src/app/api/foods/[name]/route.ts
@@ -1,31 +1,24 @@
 import { foods } from "@/data";
-import { use } from "react";
 
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ name: string }> }
 ) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const {name} = use(params);
+  const { name } = await params;
+
   const index = foods.findIndex(
     (food) => food.name.toLowerCase().replace(/ /g, "-") === name
   );
-  if (index != -1) {
+
+  if (index !== -1) {
     return new Response(JSON.stringify(foods[index]), {
-      headers: {
-        "content-type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       status: 200,
     });
-  } else {
-    return (
-      new Response("Food not found"),
-      {
-        headers: {
-          "content-type": "application/json",
-        },
-        status: 404,
-      }
-    );
   }
+
+  return new Response(JSON.stringify({ error: "Food not found" }), {
+    headers: { "Content-Type": "application/json" },
+    status: 404,
+  });
 }
