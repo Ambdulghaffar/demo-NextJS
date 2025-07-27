2db705986e312166bb16ab273f325415258768d3
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-07-27 21:01:29 +0100

    food page commit

--- src/app/food/[name]/page.tsx
+++ src/app/food/[name]/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect, use } from "react";
 import { IFood, IMacronutrientData } from "@/types";
 import { useRouter } from "next/navigation";
 import { Undo2 } from "lucide-react";
-import { Cell, Pie, PieChart } from "recharts";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
 const FoodPage = ({ params }: { params: Promise<{ name: string }> }) => {
   const { name } = use(params);
@@ -47,25 +47,48 @@ const FoodPage = ({ params }: { params: Promise<{ name: string }> }) => {
   }, [name]);
 
   return (
-    <PieChart width={800} height={400}>
-      <Pie
-        data={macronutrinments}
-        cx={120}
-        cy={200}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={5}
-        dataKey="value"
-      >
-        {macronutrinments.map((entry, index) => (
-          <Cell
-            key={`cell-${entry.name}`}
-            fill={COLORS[index % COLORS.length]}
+    <>
+      {!isLoading && food && macronutrinments ? (
+        <div className="p-8 text-white">
+          <Undo2
+            className="cursor-pointer mb-5 text-white"
+            onClick={() => router.back()}
           />
-        ))}
-      </Pie>
-    </PieChart>
+          <h1 className="mb-6 text-5xl font-bold leading-tight tracking-tight text-white md:text-6xl lg: text-7xl">
+            {food.name}
+          </h1>
+          <div className=" flex flex-col md:flex-row items-center md:items-start ">
+            <div className="w-full md:w-1/2 lg:-1/3 mb-8 md:mb-0">
+              <ResponsiveContainer width="100%" height={200}>
+                <PieChart>
+                  <Pie
+                    data={macronutrinments}
+                    cx="50%"
+                    cy="50%"
+                    innerRadius={60}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    paddingAngle={5}
+                    dataKey="value"
+                  >
+                    {macronutrinments.map((entry, index) => (
+                      <Cell
+                        key={`cell-${entry.name}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-screen text-white">
+          <p className="text-2xl">Loading...</p>
+        </div>
+      )}
+    </>
   );
 };
 
